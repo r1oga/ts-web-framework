55288fb2d93214426a743d884ac9421d238ed854
r1oga <r1oga@tuta.io>
2020-06-22 22:01:57 +0200

    feat: Can bind events to HTML Element

--- src/mod.ts
+++ src/mod.ts
@@ -1,10 +1,5 @@
-import { Collection } from './models/Collection.ts'
-import { User } from './models/User.ts'
-import { UserProps } from './models/interfaces.ts'
-import { API_URL } from './config.js'
-;(async () => {
-  const userCollection = User.buildUserCollection()
-  userCollection.on('change', () => console.log('changed'))
-  await userCollection.fetch()
-  console.log(userCollection.models)
-})()
+import { UserForm } from './views/UserForm.ts'
+
+const userForm = new UserForm(document.getElementById('root'))
+
+userForm.render()
---
+++ src/views/UserForm.ts
@@ -0,0 +1,45 @@
+export class UserForm {
+  constructor(public parent: Element) {}
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick
+    }
+  }
+
+  onButtonClick(): void {
+    console.log('Hello')
+  }
+
+  // generate HTML string
+  template(): string {
+    return `
+    <div>
+      <h1>User Form</h1>
+      <input />
+      <button>click me</button>
+    </div>
+    `
+  }
+
+  // bind event handlers
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap()
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':')
+      fragment.querySelectorAll(selector).forEach(el => {
+        el.addEventListener(eventName, eventsMap[eventKey])
+      })
+    }
+  }
+
+  // add template HTML string to parent
+  render(): void {
+    const templateElement = document.createElement('template')
+    templateElement.innerHTML = this.template()
+
+    this.bindEvents(templateElement.content)
+    this.parent.append(templateElement.content)
+  }
+}
