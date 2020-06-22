02964147495e6cc91786516a61722cc3730408c8
r1oga <r1oga@tuta.io>
2020-06-22 23:06:17 +0200

    feat: Use User Instance as argument for UserForm constructor

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,7 @@
 import { UserForm } from './views/UserForm.ts'
+import { User } from './models/User.ts'
 
-const userForm = new UserForm(document.getElementById('root'))
+const user = User.buildUser({ name: 'r1oga', age: 20 })
+const userForm = new UserForm(document.getElementById('root'), user)
 
 userForm.render()
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,5 +1,7 @@
+import { User } from '../models/User.ts'
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {}
 
   eventsMap(): { [key: string]: () => void } {
     return {
@@ -21,6 +23,8 @@ export class UserForm {
     return `
     <div>
       <h1>User Form</h1>
+      <div>User Name: ${this.model.get('name')}</div>
+      <div>User Age: ${this.model.get('age')}</div>
       <input />
       <button>click me</button>
     </div>
